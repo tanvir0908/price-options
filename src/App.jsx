9abe120c38036921b31067bcb2100cca346fda82
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-09-19 03:42:26 +0600

    React Chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import LineChart from "./components/LineChart/LineChart";
 // import DaisyNav from "./components/DaisyNav/DaisyNav";
 import Navbar from "./components/Navbar/Navbar";
 import PriceOptions from "./components/PriceOptions/PriceOptions";
@@ -9,6 +10,7 @@ function App() {
       {/* <DaisyNav /> */}
       <Navbar />
       <PriceOptions />
+      <LineChart />
     </>
   );
 }
